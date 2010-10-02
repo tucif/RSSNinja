cf4b3ab1e9b52e2a49664cc79345e1dc6ccf8d5b
Rafael Santos <rsantos@xone.(none)>
2010-10-02 17:57:45 -0500

    json parser

--- com/rssninja/Aprendiz.java
+++ com/rssninja/Aprendiz.java
@@ -1,17 +1,26 @@
 package com.rssninja;
-import java.util.*;
+//import java.util.*;
 import jadex.runtime.*;
+import org.json.simple.*;
 
-public class Aprendiz extends Plan{
-	
-	public Aprendiz(){
-	    System.out.println("Aprendiz creado");
-	}
-	
-	public void body(){
-		IMessageEvent message = (IMessageEvent)getInitialEvent();
-		String word = (String)message.getContent();
-		System.out.println(word);
-	}
+public class Aprendiz extends Plan {
+
+    public Aprendiz() {
+        System.out.println("Aprendiz creado");
+    }
+
+    @Override
+    public void body() {
+        IMessageEvent message = (IMessageEvent) getInitialEvent();
+        String content = (String) message.getContent();
+        System.out.println(content);
+
+        String s="[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
+        Object obj = JSONValue.parse(content);
+        JSONArray array = (JSONArray) obj;
+        System.out.println("======the 2nd element of array======");
+        System.out.println(array.get(1));
+        System.out.println();
+
+    }
 }
-
