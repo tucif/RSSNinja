21dd6bc71fa2ddb5f4c59c9a7d41ac096f2895df
Rafael Santos <rsantos@xone.(none)>
2010-10-02 18:36:18 -0500

    Mensajes JSON recibidos por aprendiz y contestados con performativa inform

--- com/rssninja/Aprendiz.java
+++ com/rssninja/Aprendiz.java
@@ -15,12 +15,15 @@ public class Aprendiz extends Plan {
         String content = (String) message.getContent();
         System.out.println(content);
 
-        String s="[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
-        Object obj = JSONValue.parse(content);
-        JSONArray array = (JSONArray) obj;
-        System.out.println("======the 2nd element of array======");
-        System.out.println(array.get(1));
+        //message format: {"service": "twitter","tag": "teambj"}
+        JSONObject obj = (JSONObject)JSONValue.parse(content);
+        System.out.println("======parsed input======");
+        System.out.println(obj.get("service"));
+        System.out.println(obj.get("tag"));
         System.out.println();
+
+        sendMessage(message.createReply("inform", "OK"));
+
 
     }
 }
