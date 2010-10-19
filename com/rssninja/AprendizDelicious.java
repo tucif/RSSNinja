083b04bbcc1b62280a87cb4f68671f15e1653401
Jonathan Valle <zheroth@Okami.(none)>
2010-10-19 18:00:08 -0500

    Delicious agent brings json info from Delicious

--- com/rssninja/AprendizDelicious.java
+++ com/rssninja/AprendizDelicious.java
@@ -2,9 +2,16 @@ package com.rssninja;
 //import java.util.*;
 
 import jadex.runtime.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.json.simple.*;
 import jadex.adapter.fipa.SFipa;
 
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class AprendizDelicious extends Plan {
 
@@ -21,7 +28,42 @@ public class AprendizDelicious extends Plan {
         System.out.println("tag="+tag);
         System.out.println();
 
-        //TODO: search delicious for tag
+        String popularUrlString="http://feeds.delicious.com/v2/json/popular/"+tag;
+        String recentUrlString="http://feeds.delicious.com/v2/json/tag/"+tag;
+
+        URL popularUrl=null;
+        URL recentUrl=null;
+        
+        BufferedReader br=null;
+
+        try {
+            popularUrl = new URL(popularUrlString);
+            recentUrl = new URL(recentUrlString);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(AprendizDelicious.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            br = new BufferedReader(new InputStreamReader(popularUrl.openStream()));
+        } catch (IOException ex) {
+            Logger.getLogger(AprendizDelicious.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            System.out.println(br.readLine());
+        } catch (IOException ex) {
+            Logger.getLogger(AprendizDelicious.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            br = new BufferedReader(new InputStreamReader(recentUrl.openStream()));
+        } catch (IOException ex) {
+            Logger.getLogger(AprendizDelicious.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            System.out.println(br.readLine());
+        } catch (IOException ex) {
+            Logger.getLogger(AprendizDelicious.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
         //TODO: format info from delicious
 
