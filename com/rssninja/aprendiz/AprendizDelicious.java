5eae6eadcf766922d55f4204d323919aa6d7d53e
Jonathan Valle <zheroth@Okami.(none)>
2010-10-21 18:27:08 -0500

    Added utils package, refactored code

--- com/rssninja/aprendiz/AprendizDelicious.java
+++ com/rssninja/aprendiz/AprendizDelicious.java
@@ -1,16 +1,8 @@
 package com.rssninja.aprendiz;
 
+import com.rssninja.utils.UrlManager;
 import jadex.runtime.*;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.json.simple.*;
-import jadex.adapter.fipa.SFipa;
-
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class AprendizDelicious extends Plan {
 
@@ -26,34 +18,11 @@ public class AprendizDelicious extends Plan {
         String popularUrlString="http://feeds.delicious.com/v2/json/popular/"+tag+"?count=4";
         String recentUrlString="http://feeds.delicious.com/v2/json/tag/"+tag+"?count=4";
 
-        JSONArray popularLinks = getJSONFromURL(popularUrlString);
-        JSONArray recentLinks = getJSONFromURL(recentUrlString);
+        JSONArray popularLinks = UrlManager.getJSONArrFromURL(popularUrlString);
+        JSONArray recentLinks = UrlManager.getJSONArrFromURL(recentUrlString);
 
         sendMessagesToNinja(popularLinks, message);
         sendMessagesToNinja(recentLinks,message);
-    }
-
-    public JSONArray getJSONFromURL(String urlString){
-        URL url=null;
-        BufferedReader br=null;
-        JSONArray jsonArray = null;
-        try {
-            url = new URL(urlString);
-        } catch (MalformedURLException ex) {
-            //TODO: do something when this happens
-        }
-        try {
-            br = new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch (IOException ex) {
-            //TODO: do something when this happens
-        }
-        try {
-            jsonArray=(JSONArray)JSONValue.parse(br.readLine());
-        } catch (IOException ex) {
-            //TODO: do something when this happens
-        }
-
-        return jsonArray;
     }
 
     public void sendMessagesToNinja(JSONArray jsonArray, IMessageEvent message){
@@ -70,7 +39,7 @@ public class AprendizDelicious extends Plan {
 
             objectToSend.put("service","delicious");
             objectToSend.put("link",urlString);
-            objectToSend.put("content",getContentFromURL(urlString));
+            objectToSend.put("content",UrlManager.getContentFromURL(urlString));
 
             //generate Meta
             metaData=new JSONObject();
@@ -86,37 +55,4 @@ public class AprendizDelicious extends Plan {
 
         }
     }
-
-    public String getContentFromURL(String urlString){
-        BufferedReader br = null;
-        StringBuilder sb= new StringBuilder("");
-        URL url=null;
-        try {
-            url = new URL(urlString);
-        } catch (MalformedURLException ex) {
-            //TODO: do something when this happens
-            System.out.println("MalformedURL: "+urlString.toString());
-        }
-
-        try {
-            br= new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch (IOException ex) {
-            //TODO: do something when this happens
-        }
-        try {
-            sb= new StringBuilder("");
-            String nextLine="";
-            while(true){
-                nextLine=br.readLine();
-                if(nextLine==null)
-                    break;
-                sb.append(nextLine);
-            }
-        } catch (IOException ex) {
-            //TODO: do something when this happens
-        }
-
-        return sb.toString();
-    }
-
 }
--- com/rssninja/aprendiz/AprendizWikipedia.java
+++ com/rssninja/aprendiz/AprendizWikipedia.java
@@ -1,14 +1,9 @@
 package com.rssninja.aprendiz;
 
+import com.rssninja.utils.UrlManager;
 import jadex.runtime.*;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import org.json.simple.*;
-import jadex.adapter.fipa.SFipa;
 
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class AprendizWikipedia extends Plan {
 
@@ -23,7 +18,7 @@ public class AprendizWikipedia extends Plan {
 
         String url="http://en.wikipedia.org/w/index.php?action=raw&title="+tag;
 
-        String wikiText = getContentFromURL(url);
+        String wikiText = UrlManager.getContentFromURL(url);
 
         JSONObject objectToSend=new JSONObject();
         objectToSend.put("service","wikipedia");
@@ -38,37 +33,4 @@ public class AprendizWikipedia extends Plan {
         sendMessage(message.createReply("inform",objectToSend.toString()));
 
     }
-
-    public String getContentFromURL(String urlString){
-        BufferedReader br = null;
-        StringBuilder sb= new StringBuilder("");
-        URL url=null;
-        try {
-            url = new URL(urlString);
-        } catch (MalformedURLException ex) {
-            //TODO: do something when this happens
-            System.out.println("MalformedURL: "+urlString.toString());
-        }
-
-        try {
-            br= new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch (IOException ex) {
-            //TODO: do something when this happens
-        }
-        try {
-            sb= new StringBuilder("");
-            String nextLine="";
-            while(true){
-                nextLine=br.readLine();
-                if(nextLine==null)
-                    break;
-                sb.append(nextLine);
-            }
-        } catch (IOException ex) {
-            //TODO: do something when this happens
-        }
-
-        return sb.toString();
-    }
-
 }
