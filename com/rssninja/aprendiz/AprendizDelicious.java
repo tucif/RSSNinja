2bf42db6356bf1fe936cf88c51640080e56a4485
Jonathan Valle <zheroth@Okami.(none)>
2010-10-21 17:39:11 -0500

    Grouped agents in packages

---
+++ com/rssninja/aprendiz/AprendizDelicious.java
@@ -0,0 +1,122 @@
+package com.rssninja.aprendiz;
+
+import jadex.runtime.*;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.*;
+import jadex.adapter.fipa.SFipa;
+
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class AprendizDelicious extends Plan {
+
+    public AprendizDelicious() {
+        System.out.println("AprendizDelicious creado");
+    }
+
+    @Override
+    public void body(){
+        IMessageEvent message = (IMessageEvent) getInitialEvent();
+        String tag = (String) message.getContent();
+
+        String popularUrlString="http://feeds.delicious.com/v2/json/popular/"+tag+"?count=4";
+        String recentUrlString="http://feeds.delicious.com/v2/json/tag/"+tag+"?count=4";
+
+        JSONArray popularLinks = getJSONFromURL(popularUrlString);
+        JSONArray recentLinks = getJSONFromURL(recentUrlString);
+
+        sendMessagesToNinja(popularLinks, message);
+        sendMessagesToNinja(recentLinks,message);
+    }
+
+    public JSONArray getJSONFromURL(String urlString){
+        URL url=null;
+        BufferedReader br=null;
+        JSONArray jsonArray = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException ex) {
+            //TODO: do something when this happens
+        }
+        try {
+            br = new BufferedReader(new InputStreamReader(url.openStream()));
+        } catch (IOException ex) {
+            //TODO: do something when this happens
+        }
+        try {
+            jsonArray=(JSONArray)JSONValue.parse(br.readLine());
+        } catch (IOException ex) {
+            //TODO: do something when this happens
+        }
+
+        return jsonArray;
+    }
+
+    public void sendMessagesToNinja(JSONArray jsonArray, IMessageEvent message){
+        String urlString=null;
+        JSONObject currentObject= null;
+        JSONObject objectToSend=null;
+        JSONObject metaData = null;
+        
+        for(int i=0; i<jsonArray.size(); i++){
+            objectToSend=new JSONObject();
+            currentObject= (JSONObject)jsonArray.get(i);
+
+            urlString= currentObject.get("u").toString();
+
+            objectToSend.put("service","delicious");
+            objectToSend.put("link",urlString);
+            objectToSend.put("content",getContentFromURL(urlString));
+
+            //generate Meta
+            metaData=new JSONObject();
+            metaData.put("type","html");
+            metaData.put("title",currentObject.get("d").toString());
+            metaData.put("date",currentObject.get("dt").toString());
+            metaData.put("relatedTags",currentObject.get("t").toString());
+
+            objectToSend.put("meta",metaData.toString());
+            System.out.println("metadata: "+metaData.toString());
+
+            sendMessage(message.createReply("inform",objectToSend.toString()));
+
+        }
+    }
+
+    public String getContentFromURL(String urlString){
+        BufferedReader br = null;
+        StringBuilder sb= new StringBuilder("");
+        URL url=null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException ex) {
+            //TODO: do something when this happens
+            System.out.println("MalformedURL: "+urlString.toString());
+        }
+
+        try {
+            br= new BufferedReader(new InputStreamReader(url.openStream()));
+        } catch (IOException ex) {
+            //TODO: do something when this happens
+        }
+        try {
+            sb= new StringBuilder("");
+            String nextLine="";
+            while(true){
+                nextLine=br.readLine();
+                if(nextLine==null)
+                    break;
+                sb.append(nextLine);
+            }
+        } catch (IOException ex) {
+            //TODO: do something when this happens
+        }
+
+        return sb.toString();
+    }
+
+}
---
+++ com/rssninja/aprendiz/AprendizLastFM.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.rssninja.aprendiz;
+
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+import org.json.simple.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+/**
+ *
+ * @author unicorn
+ */
+public class AprendizLastFM extends Plan{
+
+    public AprendizLastFM(){
+        System.out.println("Aprendiz LastFM creado!");
+    }
+    @Override
+    public void body() {
+        IMessageEvent message = (IMessageEvent) getInitialEvent();
+        String tag = (String) message.getContent();
+
+        String url = "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&format=json&&api_key=b25b959554ed76058ac220b7b2e0a026&artist="+tag;
+        SendMessagesToNinja(getTopAlbums(url), message);
+    }
+    public JSONArray getTopAlbums(String source){
+        URL url = null;
+        BufferedReader r = null;
+        JSONArray albums = null;
+        try {
+            url = new URL(source);
+            
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            r = new BufferedReader(new InputStreamReader(url.openStream()));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        JSONObject json = (JSONObject)JSONValue.parse(r);        
+        if(json.get("error")==null){
+            JSONObject topAlbums = (JSONObject)json.get("topalbums");
+            if(topAlbums.get("album")==null){
+                //Artist but no album
+                System.out.println("No tiene album");
+                return null;
+            }else{
+                albums = (JSONArray) topAlbums.get("album");
+                return albums;
+            }
+        }else{
+            //No artis with that tag
+            System.out.println("no tiene artista");
+            return null;
+        }
+    }
+    public void SendMessagesToNinja(JSONArray albums,IMessageEvent message){
+        JSONObject album = null;
+        JSONObject metadata = null;
+        if(albums != null){
+            for(Object i : albums){
+                album = new JSONObject();
+                album.put("service","lastFM");
+                album.put("link", (String)((JSONObject)i).get("url"));
+                album.put("content",((JSONObject)i).get("name"));
+                metadata = new JSONObject();
+                metadata.put("type", "fulltext");
+                metadata.put("author", ((JSONObject)((JSONObject)i).get("artist")).get("name"));
+                metadata.put("date", "");
+                album.put("metadata", metadata);
+                sendMessage(message.createReply("inform",album.toString()));
+            }
+        }else{
+            /*
+             TODO:check the json format when an error ocurrs
+             */
+            album = new JSONObject();
+            album.put("service", "lastFM");
+            album.put("link", "none");
+            album.put("content", "none");
+            metadata = new JSONObject();
+            metadata.put("type", "null");
+            metadata.put("author", "none");
+            metadata.put("date", "10/10/10");
+            album.put("metadata", metadata);
+            sendMessage(message.createReply("inform",album.toString()));
+        }
+    }
+}
