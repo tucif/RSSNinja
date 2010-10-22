a1e9e6c4e559a3a8ae23fe86338dca96e54db92d
Zheroth <riaj06@gmail.com>
2010-10-21 20:47:00 -0500

    refactored AprendizTwitterSearch, removed unused imports

--- com/rssninja/aprendiz/AprendizTwitterSearch.java
+++ com/rssninja/aprendiz/AprendizTwitterSearch.java
@@ -1,9 +1,7 @@
 package com.rssninja.aprendiz;
 
+import com.rssninja.utils.UrlManager;
 import jadex.runtime.*;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
 import org.json.simple.*;
 
 /**
@@ -22,30 +20,18 @@ public class AprendizTwitterSearch extends Plan {
         IMessageEvent message = (IMessageEvent) getInitialEvent();
         String content = (String) message.getContent();
         System.out.println(content);
+        String dict = "http://search.twitter.com/search.json?q="+content;
 
-        try{
-        URL dict = new URL("http://search.twitter.com/search.json?q="+content);
-        BufferedReader in = new BufferedReader(new InputStreamReader(dict.openStream()));
-        //String inline;
         System.out.println("respuesta recibida");
 
-        JSONObject response = (JSONObject) JSONValue.parse(in);
+        JSONObject response = UrlManager.getJSONObjFromURL(dict);
         JSONArray results = (JSONArray) response.get("results");
 
         for(int i = 0; i< results.size(); i++){
-        System.out.println(((JSONObject)results.get(i)).get("text"));
-        System.out.println();
+            System.out.println(((JSONObject)results.get(i)).get("text"));
+            System.out.println();
         }
-
+        
         sendMessage(message.createReply("inform", "OK"));
-        //        while ((inline = in.readLine()) != null) {
-        //            System.out.println(inline);
-        //        }
-        //        in.close();
-        //        sendMessage(message.createReply("inform", "OK"));
-        }catch(Exception e){
-        System.out.println(e);
-        sendMessage(message.createReply("inform","Could not understand: "+content));
-        }      
     }
 }
