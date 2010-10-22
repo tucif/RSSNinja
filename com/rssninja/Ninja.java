9d0b4b1d74732b9dd46b73e35d4960445959515d
Zheroth <riaj06@gmail.com>
2010-10-22 01:36:15 -0500

    Ninja receives json object and creates Aprendices to search in services

--- com/rssninja/Ninja.java
+++ com/rssninja/Ninja.java
@@ -8,35 +8,54 @@ package com.rssninja;
 import jadex.adapter.fipa.AgentIdentifier;
 import jadex.runtime.*;
 import jadex.adapter.fipa.SFipa;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
 /**
  *
  * @author unicorn
  */
 public class Ninja extends Plan{
+
+    private static String[] APPRENTICE_ARRAY={  "Delicious",
+                                                "LastFM",
+                                                "Twitpic",
+                                                "TwitterSearch",
+                                                "Wikipedia"};
     public Ninja() {
         System.out.println("Ninja creado");
     }
+    
     @Override
     public void body(){
         IMessageEvent message = (IMessageEvent) getInitialEvent();
-        String content = (String) message.getContent();
-        System.out.println(content);
-        int count = Integer.parseInt(content);
-
-        while(count>0){
-            sendMessage(message.createReply(SFipa.INFORM, "OK"));
-            count --;
-            IGoal ca = createGoal("ams_create_agent");
-            ca.getParameter("type").setValue("com.rssninja.Aprendiz");
-            dispatchSubgoalAndWait(ca);
-            AgentIdentifier createdAgent  = (AgentIdentifier)ca.getParameter("agentidentifier").getValue();
-
-            IMessageEvent ime = createMessageEvent("poll");
-            ime.setContent("{\"service\":\"twitter\",\"tag\":\"paradamamdasd\"}");
-            ime.getParameterSet(SFipa.RECEIVERS).addValue(createdAgent);
-            sendMessage(ime);
+        String contentStr = (String) message.getContent();
+        System.out.println(contentStr);
+        
+        //Json format: {"search":"tag"}
+        JSONObject content = (JSONObject)JSONValue.parse(contentStr);
+        if(content.containsKey("search")){
+           String tag = (String)content.get("search");
+           sendTagToAll(tag);
         }
     }
 
+    private void sendTagToAll(String tag){
+        //Create message
+        IMessageEvent ime = createMessageEvent("search");
+        ime.setContent(tag);
+
+        //Add receivers to message
+        for(String name:Ninja.APPRENTICE_ARRAY){
+            String path="com.rssninja.aprendiz.Aprendiz"+name;
+            IGoal ca = createGoal("ams_create_agent");
+            ca.getParameter("type").setValue(path);
+            dispatchSubgoalAndWait(ca);
+            AgentIdentifier createdAgent = (AgentIdentifier)ca.getParameter("agentidentifier").getValue();
+            ime.getParameterSet(SFipa.RECEIVERS).addValue(createdAgent);
+        }
+
+        //Send message
+        sendMessage(ime);
+    }
 }
