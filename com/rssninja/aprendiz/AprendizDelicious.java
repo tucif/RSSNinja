a1d7c303cb19eec5e23c4c54a182c4c902a9e386
Tucif <tucif.x@gmail.com>
2010-10-21 21:02:24 -0500

    changed delicious agent to avoid unchecked operations

--- com/rssninja/aprendiz/AprendizDelicious.java
+++ com/rssninja/aprendiz/AprendizDelicious.java
@@ -2,6 +2,7 @@ package com.rssninja.aprendiz;
 
 import com.rssninja.utils.UrlManager;
 import jadex.runtime.*;
+import java.util.HashMap;
 import org.json.simple.*;
 
 public class AprendizDelicious extends Plan {
@@ -14,7 +15,7 @@ public class AprendizDelicious extends Plan {
     public void body(){
         IMessageEvent message = (IMessageEvent) getInitialEvent();
         String tag = (String) message.getContent();
-
+        System.out.println(tag);
         String popularUrlString="http://feeds.delicious.com/v2/json/popular/"+tag+"?count=4";
         String recentUrlString="http://feeds.delicious.com/v2/json/tag/"+tag+"?count=4";
 
@@ -28,11 +29,14 @@ public class AprendizDelicious extends Plan {
     public void sendMessagesToNinja(JSONArray jsonArray, IMessageEvent message){
         String urlString=null;
         JSONObject currentObject= null;
-        JSONObject objectToSend=null;
-        JSONObject metaData = null;
-        
+        HashMap<String,String> objectToSend=null;
+        HashMap<String,String> metaData = null;
+
+
+
+
         for(int i=0; i<jsonArray.size(); i++){
-            objectToSend=new JSONObject();
+            objectToSend=new HashMap<String,String>();
             currentObject= (JSONObject)jsonArray.get(i);
 
             urlString= currentObject.get("u").toString();
@@ -42,17 +46,21 @@ public class AprendizDelicious extends Plan {
             objectToSend.put("content",UrlManager.getContentFromURL(urlString));
 
             //generate Meta
-            metaData=new JSONObject();
+            metaData=new HashMap<String,String>();
+
             metaData.put("type","html");
-            metaData.put("title",currentObject.get("d").toString());
-            metaData.put("date",currentObject.get("dt").toString());
+            metaData.put("title",(String)currentObject.get("d"));
+            metaData.put("date",(String)currentObject.get("dt"));
             metaData.put("relatedTags",currentObject.get("t").toString());
 
-            objectToSend.put("meta",metaData.toString());
-            System.out.println("metadata: "+metaData.toString());
+            String metaDataStr = JSONObject.toJSONString(metaData);
+            objectToSend.put("meta",metaDataStr);
+            System.out.println("metadata: "+metaDataStr);
 
-            sendMessage(message.createReply("inform",objectToSend.toString()));
+            String jsonResult = JSONValue.toJSONString(objectToSend);
 
+            sendMessage(message.createReply("inform",jsonResult));
+            System.out.println("message sent");
         }
     }
 }
