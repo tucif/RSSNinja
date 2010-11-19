24fe778a549f3ad229cfab2d08932141a6e502c6
Zheroth <riaj06@gmail.com>
2010-11-18 19:02:07 -0600

    Added missing plan

---
+++ com/rssninja/ninja/SearchInternetPlan.java
@@ -0,0 +1,58 @@
+package com.rssninja.ninja;
+
+import jadex.adapter.fipa.AgentIdentifier;
+import jadex.runtime.*;
+import jadex.adapter.fipa.SFipa;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+/**
+ *
+ * @author unicorn
+ */
+public class SearchInternetPlan extends Plan{
+
+    private static String[] APPRENTICE_ARRAY={  "Delicious",
+                                                "LastFM",
+                                                "Twitpic",
+                                                "TwitterSearch",
+                                                "Wikipedia"};
+    public SearchInternetPlan() {
+        System.out.println("Buscando en internet");
+    }
+    
+    @Override
+    public void body(){
+        IMessageEvent message = (IMessageEvent) getInitialEvent();
+        String contentStr = (String) message.getContent();
+
+        //Json format: {"search":"tag"}
+        JSONObject content = (JSONObject)JSONValue.parse(contentStr);
+        if(content.containsKey("search")){
+           String tag = (String)content.get("search");
+           sendTagToAll(tag);
+        }else{
+            sendMessage(message.createReply("informSend","Could not understand: "+message.getContent()));
+        }
+    }
+
+    private void sendTagToAll(String tag){
+        //Create message
+        IMessageEvent ime = createMessageEvent("search");
+        ime.setContent(tag);
+
+        //Add receivers to message
+        for(String name:SearchInternetPlan.APPRENTICE_ARRAY){
+            String path="com.rssninja.aprendiz.Aprendiz"+name;
+            IGoal ca = createGoal("ams_create_agent");
+            ca.getParameter("type").setValue(path);
+            dispatchSubgoalAndWait(ca);
+          
+            AgentIdentifier createdAgent = (AgentIdentifier)ca.getParameter("agentidentifier").getValue();
+            ime.getParameterSet(SFipa.RECEIVERS).addValue(createdAgent);
+        }
+
+        //Send message
+        sendMessage(ime);
+    }
+}
